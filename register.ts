26a18cf8562cb64904c4ce26bfa34ce6e8f7b9f9
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-04-19 20:04:05 -0400

    oop + latest changes

--- register.ts
+++ register.ts
@@ -1,148 +1,201 @@
-import playwright from 'playwright';
-import dotenv from 'dotenv';
+import playwright from "playwright";
+import dotenv from "dotenv";
 dotenv.config();
 
-//not my favorite stuff to keep in a global context, but it works
-let browser: playwright.Browser = await playwright.chromium.launch({ headless: false});
-let context: playwright.BrowserContext = await browser.newContext();
-let page: playwright.Page = await context.newPage();
+class Script {
+  private browser!: playwright.Browser;
+  private context!: playwright.BrowserContext;
+  private page!: playwright.Page;
+  private course: string;
+  private username: string;
+  private password: string;
 
-async function signin(): Promise<boolean> {
-    console.log("Signing in")
+  constructor(username: string, password: string, course: string) {
+    this.username = username;
+    this.password = password;
+    this.course = course;
+  }
 
-    await page.goto('https://www.myworkday.com/bentley/d/home.htmld');
-    await page.waitForSelector("text='Bentley Faculty, Staff and Students'");
+  private async init() {
+    this.browser = await playwright.chromium.launch({ headless: false });
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
+  }
 
-    await page.click("text='Bentley Faculty, Staff and Students'");
+  private async signin(): Promise<boolean> {
+    console.log("Signing in");
 
-    await page.waitForSelector('text=Sign In');
+    await this.page.goto("https://www.myworkday.com/bentley/d/home.htmld");
+    await this.page.waitForSelector(
+      "text='Bentley Faculty, Staff and Students'"
+    );
 
-    let username = process.env.USER;
-    let password = process.env.PASS;
+    await this.page.click("text='Bentley Faculty, Staff and Students'");
 
-    if (!username || !password) return true
+    await this.page.waitForSelector("text=Sign In");
 
-    await page.type('input[type=email]', username);
+    await this.page.type("input[type=email]", this.username);
 
-    await page.click("input[type=submit]");
+    await this.page.click("input[type=submit]");
 
-    await page.type('input[type=password]', password);
+    await this.page.type("input[type=password]", this.password);
 
-    await page.click('input[type=submit]');
+    await this.page.click("input[type=submit]");
 
-    await page.waitForTimeout(500);
+    await this.page.waitForTimeout(500);
 
-    if (JSON.stringify(await page.$$("div[role=heading]")) != "[]") {
-        console.log("Manual intervention required");
+    if (JSON.stringify(await this.page.$$("div[role=heading]")) != "[]") {
+      console.log("Manual intervention required");
     }
 
-    await page.waitForURL("https://www.myworkday.com/bentley/login-saml.htmld", { timeout: 120000 })
+    await this.page.waitForURL(
+      "https://www.myworkday.com/bentley/login-saml.htmld",
+      { timeout: 120000 }
+    );
 
     //fluff code because i've had issues with waitForURL before
-    if (page.url() == "https://www.myworkday.com/bentley/login-saml.htmld") {
-        console.log("Login Success");
-        return false;
+    if (
+      this.page.url() == "https://www.myworkday.com/bentley/login-saml.htmld"
+    ) {
+      console.log("Login Success");
+      return false;
     }
     return true;
-}
+  }
 
-async function goToPage(): Promise<boolean> {
+  private async goToPage(): Promise<boolean> {
     let err = true;
 
-    await page.click("text='Academics'");
+    await this.page.click("text='Academics'");
 
-    await page.click("text='More (3)'");
+    await this.page.click("text='More (3)'");
 
-    await page.click("text='View My Saved Schedules'")
+    await this.page.click("text='View My Saved Schedules'");
 
-    await page.waitForSelector("input[dir='ltr']");
+    await this.page.waitForSelector("input[dir='ltr']");
 
-    await page.click("input[dir='ltr']", { delay: 500 })
+    await this.page.click("input[dir='ltr']", { delay: 500 });
 
-    await page.click("div[data-uxi-multiselectlistitem-index='1']", { delay: 500 })
+    await this.page.click("div[data-uxi-multiselectlistitem-index='1']", {
+      delay: 500,
+    });
 
-    let course = process.env.COURSE
-    if (!course) return err
+    if (this.course == "latest") {
+      await this.page.waitForSelector("div[role=listbox]>div");
+      const parent = await this.page.$$("div[role=listbox]>div>div");
 
-    if (course == "latest") {
-        await page.waitForSelector("div[role=listbox]>div")
-        let parent = await page.$$("div[role=listbox]>div>div")
-
-        let last = await parent[parent.length - 1].textContent()
-        let secondLast = await parent[parent.length - 2].textContent()
-        // added + 1 to account for nth-child indexing starting at 1, it makes more sense in my head
-        if (secondLast?.substring(0, 5) == last?.substring(0, 5)) {
-            await page.click(`div[role=listbox]>div>div:nth-child(${parent.length - 2 + 1})`, { delay: 500 })
-        } else {
-            await page.click(`div[role=listbox]>div>div:nth-child(${parent.length - 1 + 1})`, { delay: 500 })
+      let semesters: string[] = [];
+      for (let i = 0; i < parent.length; i++) {
+        let text = await this.page.evaluate((el) => el.textContent, parent[i]);
+        if (text != null) {
+          semesters.push(text);
         }
+      }
+
+      let reversed = semesters.map((x) => x);
+      reversed.reverse();
+
+      if (semesters.length == 1)
+        await this.page.click("div[role=listbox]>div>div:nth-child(1)");
+      else {
+        for (let i = 0; i < semesters.length - 1; i++) {
+          if (reversed[i].substring(0, 5) > reversed[i + 1].substring(0, 5)) {
+            let index = semesters.indexOf(reversed[i]);
+            await this.page.click(
+              `div[role=listbox]>div>div:nth-child(${index + 1})`,
+              { delay: 500 }
+            );
+            break;
+          }
+        }
+      }
     } else {
-        await page.click("text='" + course + "'")
+      await this.page.click("text='" + this.course + "'");
     }
 
-    await page.waitForTimeout(500)
+    await this.page.waitForTimeout(500);
 
-    await page.click("button[title='OK']")
+    await this.page.click("button[title='OK']");
 
-    let urlRegex = /\/bentley\/d\/gateway\.htmld/g
-    await page.waitForURL(urlRegex, { timeout: 99000 })
-        .then(() => {
-            console.log("Got to registration page");
-            err = false
-        })
+    const urlRegex = /\/bentley\/d\/gateway\.htmld/g;
+    await this.page.waitForURL(urlRegex, { timeout: 99000 }).then(() => {
+      console.log("Got to registration page");
+      err = false;
+    });
 
-    return err
-}
+    return err;
+  }
 
-async function monitor(): Promise<string> {
-    let registerButton = await page.$$("text='Start Registration'")
+  private async monitor(): Promise<string> {
+    const registerButton = await this.page.$$("text='Start Registration'");
 
     if (JSON.stringify(registerButton) != "[]") {
-        //all speculative code based off a couple images, not sure if it's correct
-        console.log("Registration open... Registering")
-        await page.click("text='Start Registration'")
+      //all speculative code based off a couple images, not sure if it's correct
+      console.log("Registration open... Registering");
+      await this.page.click("text='Start Registration'");
 
-        await page.waitForSelector("text='Register'")
-        await page.click("text='Register'")
+      await this.page.waitForSelector("text='Register'");
+      await this.page.click("text='Register'");
 
-        await page.waitForLoadState("domcontentloaded")
+      await this.page.waitForLoadState("domcontentloaded");
 
-        if (JSON.stringify(await page.$$("text='Unsuccessful Registrations'")) != "[]") {
-            console.log("Complete registration failed");
-            return "fail";
-        } else {
-            return "success"
-        }
+      if (
+        JSON.stringify(
+          await this.page.$$("text='Unsuccessful Registrations'")
+        ) != "[]"
+      ) {
+        console.log("Complete registration failed");
+        return "fail";
+      } else {
+        return "success";
+      }
     } else {
-        //refreshes and checks again
-        console.log("Registration not started");
-        await page.reload();
-        await page.waitForTimeout(1000)
-        await page.waitForSelector("span[data-automation-id=pageHeaderTitleText]")
-        return "monitor"
+      //refreshes and checks again
+      console.log("Registration not started");
+      await this.page.reload();
+      await this.page.waitForTimeout(1000);
+      await this.page.waitForSelector(
+        "span[data-automation-id=pageHeaderTitleText]"
+      );
+      return "monitor";
     }
+  }
+
+  public async run() {
+    await this.init();
+    let err = await this.signin();
+    if (err) return;
+    err = await this.goToPage();
+    if (err) return;
+
+    let res = "monitor";
+    while (res == "monitor") {
+      res = await this.monitor();
+      switch (res) {
+        case "success":
+          console.log("Registration successful");
+          break;
+        case "fail":
+          console.log("Registration partially or completely failed");
+          break;
+        case "monitor":
+          console.log("Monitoring registration");
+          break;
+      }
+    }
+  }
 }
 
-(async () => {
-    let err = await signin();
-    if (err) return
-    err = await goToPage()
-    if (err) return
-    let res = await monitor()
+const launch = () => {
+  let username = process.env.USER;
+  let password = process.env.PASS;
+  let course = process.env.COURSE;
 
-    switch (res) {
-        case "success":
-            console.log("Registration successful");
-            break;
-        case "fail":
-            console.log("Registration partially or completely failed");
-            break;
-        case "monitor":
-            console.log("Monitoring registration");
-            //starts monitoring loop
-            while (res == "monitor") {
-                res = await monitor()
-            }
-            break;
-    }
-})();
+  if (username == undefined || password == undefined || course == undefined)
+    return;
+
+  const script = new Script(username, password, course);
+  script.run();
+};
+
+launch();
